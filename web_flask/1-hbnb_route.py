8ea0a8f3949a3323d313741637aae7311647d79b
Pauline <paulineparmigiani@gmail.com>
2023-04-04 11:02:52 +0200

    add an other route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    '''return hello HBNB '''
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def index():
+    '''return hello HBNB '''
+    return "HBNB"
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')

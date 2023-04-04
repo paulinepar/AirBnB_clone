3ab90470b2cae6835484b47bc847177f303a8f8f
Pauline <paulineparmigiani@gmail.com>
2023-04-04 16:30:34 +0200

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,8 @@ def index_1():
 
 @app.route("/c/<text>", strict_slashes=False)
 def index2(text):
-    return "C" + text.replace('_', ' ')
+    '''display 'C ' followed by the value of the text variable'''
+    return "C " + text.replace('_', ' ')
 
 
 if __name__ == '__main__':

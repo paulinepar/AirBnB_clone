c909ddee34f7712e0e48ba66b0050893fc1e26e9
Pauline <paulineparmigiani@gmail.com>
2023-04-05 10:22:51 +0200

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def index2(text):
 @app.route("/python/<text>", strict_slashes=False)
 def index3(text="is cool"):
     ''''display Python followed by the value of the text'''
-    return "Python" + text.replace('_', ' ')
+    return "Python" + "is cool".replace('_', ' ')
 
 
 if __name__ == '__main__':

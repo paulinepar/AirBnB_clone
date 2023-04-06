8ae6efdfe24c074502a88e27be0c70e5b7728d46
Pauline <paulineparmigiani@gmail.com>
2023-04-06 10:22:15 +0200

    correct ouput in  html

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,5 +40,10 @@ def index5(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def index6(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
